0ba6fa5694ffeb15641c61dadc7736aad4e0b7ed
RamRajkumar55 <ramrajkumarmca55@gmail.com>
2026-03-14 16:46:23 +0530

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -1,14 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
-
-  tags = {
-    Name        = "aluruarumullaa1"
+  bucket = "rajkumar-terraform-bucket1-2026"
+   tags = {
+    Name        = "Rajkumar-terraform-bucket1-2026"
     Environment = "dev"
   }
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "rajkumar-terraform-bucket2-2026"
+   tags = {
+    Name        = "Rajkumar-terraform-bucket2-2026"
+    Environment = "dev"
+  }
+  
 }
 
 resource "aws_s3_bucket_versioning" "bucket1_versioning" {
@@ -17,19 +34,14 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
     status = "Enabled"
   }
 }
-
-resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
-
-  tags = {
-    Name        = "arumullaaluruu1"
-    Environment = "dev"
-  }
-}
-
 resource "aws_s3_bucket_versioning" "bucket2_versioning" {
   bucket = aws_s3_bucket.bucket2.id
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket" "buckets" {
+  for_each = toset(var.bucket_names)
+  bucket   = each.key
 }
