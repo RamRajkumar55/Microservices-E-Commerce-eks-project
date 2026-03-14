b4642f809441516649f629dbea3c53d5e0722b97
RamRajkumar55 <ramrajkumarmca55@gmail.com>
2026-03-14 16:47:01 +0530

    Rename outputs for S3 bucket IDs to names

--- s3-buckets/outputs.tf
+++ s3-buckets/outputs.tf
@@ -1,9 +1,7 @@
-output "bucket1_id" {
-  description = "ID of the first S3 bucket"
-  value       = aws_s3_bucket.bucket1.id
+output "bucket1_name" {
+  value = aws_s3_bucket.bucket1.id
 }
 
-output "bucket2_id" {
-  description = "ID of the second S3 bucket"
-  value       = aws_s3_bucket.bucket2.id
+output "bucket2_name" {
+  value = aws_s3_bucket.bucket2.id
 }
