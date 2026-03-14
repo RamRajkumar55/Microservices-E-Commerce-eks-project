cf75f6e1e5afc7a799c84248d512e4d7fd6ff2db
RamRajkumar55 <ramrajkumarmca55@gmail.com>
2026-03-14 16:47:54 +0530

    Refactor S3 bucket variables to use a list
    Replaced individual bucket name variables with a list of bucket names.

--- s3-buckets/variables.tf
+++ s3-buckets/variables.tf
@@ -1,15 +1,8 @@
-variable "bucket1_name" {
-  description = "Name of the first S3 bucket"
-  type        = string
-  default     = "aluruarumullaa1"
+variable "bucket_names" {
+  type = list(string)
+  default = ["rajkumar-terraform-bucket1-2026", "rajkumar-terraform-bucket2-2026",
+   ]
 }
-
-variable "bucket2_name" {
-  description = "Name of the second S3 bucket"
-  type        = string
-  default     = "arumullaaluruu1"
-}
-
 variable "environment" {
   description = "Environment tag for the buckets"
   type        = string
